71dac60889d5c2c5f7f72decd3a580ba68b1ab24
AndrewGWMHS <goff.614615@swl.k12.oh.us>
2024-09-12 08:22:50 -0400

    all my code

--- src/YearlyMaintenanceCostCalculator.java
+++ src/YearlyMaintenanceCostCalculator.java
@@ -1,12 +1,23 @@
 import java.util.Scanner;
-
-public class Main {
+public class YearlyMaintenanceCostCalculator {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         double springCost;
         double summerCost;
         double fallCost;
         double winterCost;
+        double totalYearCost;
 
-        
+        System.out.println("Enter the cost of your maintenance during spring:");
+        springCost = scan.nextDouble();
+        System.out.println("Enter the cost of your maintenance during summer:");
+        summerCost = scan.nextDouble();
+        System.out.println("Enter the cost of your maintenance during fall:");
+        fallCost = scan.nextDouble();
+        System.out.println("Enter the cost of your maintenance during winter:");
+        winterCost = scan.nextDouble();
+
+        totalYearCost = springCost + summerCost + fallCost + winterCost;
+        System.out.println("Your yearly cost is $" + totalYearCost + ".");
     }
 }

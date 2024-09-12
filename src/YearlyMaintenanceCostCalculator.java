54f5b195248633da82af52514e88ff91b40cb182
AndrewGWMHS <goff.614615@swl.k12.oh.us>
2024-09-12 08:21:41 -0400

    Initial commit

---
+++ src/YearlyMaintenanceCostCalculator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double springCost;
+        double summerCost;
+        double fallCost;
+        double winterCost;
+
+        
+    }
+}
